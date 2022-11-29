4ec735009e8188a995f73e1c713e29317108627d
modernpacifist <modernpacifist@gmail.com>
2022-11-29 21:32:03 +0300

    added whisper to fastapi

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+import whisper
+import requests as rq
+
+from fastapi import FastAPI, File
 
 app = FastAPI()
 
@@ -11,4 +14,17 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/translate/")
+async def create_file(file: bytes = File()):
+    model = whisper.load_model("base")
+
+    url = 'https://raw.githubusercontent.com/Maksembek/urfu_soft_eng_3/main/Female.mp3'
+    doc = rq.get(url)
+    with open('voice.mp3', 'wb') as f:
+        f.write(doc.content)
+        result = model.transcribe("voice.mp3", fp16=False, language='ru')
+
+    return {"result": result["text"]}
 
