e0248626117132e3d141dea9f3fca66fb052d3ed
Maksembek <sevimax@mail.ru>
2022-11-29 21:17:17 +0300

    hard code with url

--- mainWisperMain.py
+++ mainWisperMain.py
@@ -1,8 +1,11 @@
 import whisper
+import requests
 
 model = whisper.load_model("base")
-result = model.transcribe("Alchimest.wav", fp16=False, language='ru')
-print(result["text"])
-print()
-result = model.transcribe("Male.mp3", fp16=False, language='ru')
-print(result["text"])
+
+url = 'https://raw.githubusercontent.com/Maksembek/urfu_soft_eng_3/main/Female.mp3'
+doc = requests.get(url)
+with open('voice.mp3', 'wb') as f:
+    f.write(doc.content)
+    result = model.transcribe("voice.mp3", fp16=False, language='ru')
+    print(result["text"])
