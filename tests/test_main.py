57581e2c6725fe0a34b1fccf758936328ab8893b
Maksembek <sevimax@mail.ru>
2022-12-19 21:21:39 +0300

    update test_main

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,9 @@
-from main import transcribe_to_text
 import whisper
+
+
+def transcribe_to_text(model, file):
+    result = model.transcribe(file.name, fp16=False, language='ru')
+    return result["text"]
 
 
 class TestTranscribeToText:
@@ -9,4 +13,12 @@ class TestTranscribeToText:
         with open('./tests/Male.mp3', 'r') as f:
             res = transcribe_to_text(model, f)
 
-        assert res == "Натратно спать, от родней камням быть, в этот век преступный, постижный, ни жить не чувствовать уделзовидный. Прошу молчи, не смея меня будите."
+        assert res == " Натратно спать, от родней камням быть, в этот век преступный, постижный, ни жить не чувствовать уделзовидный. Прошу молчи, не смея меня будите."
+
+    def test_female_mp3(self):
+        model = whisper.load_model("base")
+
+        with open('./tests/Female.mp3', 'r') as f:
+            res = transcribe_to_text(model, f)
+
+        assert res == " Камесыт не будешь, Мать-дество говорил о мне, хотя нарисовой бумаги вполне."
