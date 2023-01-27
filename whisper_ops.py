14352bd38d2ff5c7e3a4fc4440f145b7f13fd0e7
modernpacifist <modernpacifist@gmail.com>
2023-01-27 13:41:16 +0000

    added safety to code in whisper_ops.py

--- whisper_ops.py
+++ whisper_ops.py
@@ -5,4 +5,12 @@ base_model = whisper.load_model("base")
 
 def transcribe_to_text(file):
     result = base_model.transcribe(file.name, fp16=False, language='ru')
-    return result["text"]
+    transcribed_text = result.get("text")
+
+    if transcribed_text is None:
+        return "Audio could not be transcribed"
+
+    if len(transcribed_text) == 0:
+        return "Audio is emtpy"
+
+    return transcribed_text
