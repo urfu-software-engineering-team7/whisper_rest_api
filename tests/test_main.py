3e4867e5f26043c386e23ff9c0f2365773950984
modernpacifist <modernpacifist@gmail.com>
2023-01-27 13:06:58 +0000

    removed mp3 files from tests folder/updated tests with mp3_samples

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,13 +3,13 @@ from whisper_ops import transcribe_to_text
 
 class TestTranscribeToText:
     def test_male_mp3(self):
-        with open('./tests/Male.mp3', 'r') as f:
+        with open('./mp3_samples/Male.mp3', 'r') as f:
             res = transcribe_to_text(f)
 
         assert res == " Натратно спать, от родней камням быть, в этот век преступный, постижный, ни жить не чувствовать уделзовидный. Прошу молчи, не смея меня будите."
 
     def test_female_mp3(self):
-        with open('./tests/Female.mp3', 'r') as f:
+        with open('./mp3_samples/Female.mp3', 'r') as f:
             res = transcribe_to_text(f)
 
         assert res == " Камесыт не будешь, Мать-дество говорил о мне, хотя нарисовой бумаги вполне."
